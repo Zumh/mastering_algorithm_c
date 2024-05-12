9d518ac0d3c7940043e47e99d5204fa1d16cd851
Zumh <17211423+Zumh@users.noreply.github.com>
2024-05-12 12:54:30 -0400

    how to define variable in c

--- advanced_c/auto/src/main.c
+++ advanced_c/auto/src/main.c
@@ -4,7 +4,7 @@ int main() {
     printf("Get number from user and double it\n");
     // auto is not needed because variable is already auto
     auto int  twice;
-
+    // storage_class var_data_type var_name
     twice = multiply(10.0);
     printf("The double is %.3d\n", twice);
     return 0;
