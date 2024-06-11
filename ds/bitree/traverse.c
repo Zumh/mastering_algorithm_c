63e577abef0a8b67a3bbbdcf09e28acc51dba8d2
Zumh <17211423+Zumh@users.noreply.github.com>
2024-06-11 10:17:07 -0400

    binary tree traversing

--- ds/bitree/traverse.c
+++ ds/bitree/traverse.c
@@ -56,6 +56,24 @@ int postorder(const BiTreeNode *node, List *list){
     // load the list with a postorder listing of the tree.
 
     if(!bitree_is_eob(node)){
-        
+        if(!bitree_is_eob(bitree_left(node))){
+            if(postorder(bitree_left(node), list) != 0){
+                return -1;
+            }
+        }
+
+            
+        if(!bitree_is_eob(bitree_right(node))){
+            if(postorder(bitree_right(node), list) != 0){
+                return -1;
+            }
+        }
+
+        if(list_ins_next(list, list_tail(list), bitree_data(node)) != 0){
+            return -1;
+        }
     }
+
+
+    return 0;
 }
