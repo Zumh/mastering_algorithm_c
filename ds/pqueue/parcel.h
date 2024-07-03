5158b23f7ef97720e51a73097763f33f1f491645
Zumh <17211423+Zumh@users.noreply.github.com>
2024-07-03 08:09:54 -0400

    update for parcel

---
+++ ds/pqueue/parcel.h
@@ -0,0 +1,10 @@
+// File parcel.h
+// priority queue example 
+#ifndef PARCEL_H
+#define PARCEL_H
+// Define a parcel strucuture for demonstration purposes.
+typedef struct Parcel {
+	int priority;
+
+} Parcel;
+#endif
